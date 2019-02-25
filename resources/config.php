4743e85382824b3c13559e810451b15edc00cb16
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-02-25 11:05:09 -1000

    category page and function complete

--- resources/config.php
+++ resources/config.php
@@ -1,7 +1,25 @@
 <?php 
 
+ob_start();
+
+session_start();
+
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
-echo __DIR__;
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates" . DS . "front");
+
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates" . DS . "back");
+
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+
+defined("DB_USER") ? null : define("DB_USER", "root");
+
+defined("DB_PASS") ? null : define("DB_PASS", "");
+
+defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+require_once("functions.php");
 
 ?>
