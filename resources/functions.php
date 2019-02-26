e7dd3724524749683b1f0683f0c567927a3f5e83
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-02-26 12:55:17 -1000

    product function complete

--- resources/functions.php
+++ resources/functions.php
@@ -34,6 +34,7 @@ function fetch_array($result){
 
     return mysqli_fetch_array($result);
 }
+// FRONT END FUNCTIONS
 
 //Get products
 
@@ -51,7 +52,7 @@ $product = <<<DELIMETER
         <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
         <div class="caption">
             <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-            <h4><a href="product.html">{$row['product_title']}</a>
+            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
             </h4>
             <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
             <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
@@ -81,5 +82,66 @@ echo $categories_links;
 
     }
 }
+
+
+function get_products_in_cat_page(){
+
+$query = query("SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']) . " ");
+confirm($query);
+
+while($row = fetch_array($query)){
+
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+    <div class="thumbnail">
+        <img src="{$row['product_image']}" alt="">
+        <div class="caption">
+            <h3>{$row['product_title']}</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p>
+                <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+DELIMETER;
+
+    echo $product;
+}
+
+}
+
+function get_products_in_shop_page(){
+
+$query = query("SELECT * FROM products");
+confirm($query);
+
+while($row = fetch_array($query)){
+
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+    <div class="thumbnail">
+        <img src="{$row['product_image']}" alt="">
+        <div class="caption">
+            <h3>{$row['product_title']}</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p>
+                <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+DELIMETER;
+
+    echo $product;
+}
+
+}
+
+// BACK END FUNCTIONS
 
 ?>
